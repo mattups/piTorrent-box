1063bd84c4110ee6f5f9ef6df04bd1bcce9eed75
mattups <tupone.mattia@gmail.com>
2017-04-24 12:39:47 +0200

    Completed parameters gathering

--- piT-box-transmission.sh
+++ piT-box-transmission.sh
@@ -14,9 +14,8 @@ sudo /etc/init.d/transmission-daemon stop
 
 #Backup actual configuration
 sudo cp /etc/transmission-daemon/settings.json /etc/transmission-daemon/settings.json.original
-sudo nano /etc/transmission-daemon/settings.json
 
-#TODO ask user for changes
+#Getting configuration parameters from user's input
 read -p "Where do you want to store your downloads? " download_dir 
 read -p "Where do you want to store your incomplete downloads? " incomplete_dir
 read -p "What's user you want to use for login? " rpc_username
@@ -28,6 +27,8 @@ config_incomplete_dir="\"incomplete-dir\": \"${incomplete_dir}\","
 config_rpc_password="\"rpc-password\": \"${rpc_password}\","
 config_rpc_username="\"rpc-username\": \"${rpc_username}\","
 config_rpc_whitelist="\"rpc-whitelist\": \"${rpc_whitelist}\","
+
+#TODO Write changes to /etc/transmission-daemon/settings.json
 
 #Restart daemon
 sudo /etc/init.d/transmission-daemon start
